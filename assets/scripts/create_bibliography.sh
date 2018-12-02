e9268ab3e9071d1fb029b85cd5a40c92ee1a64c1
Daren Card <darencard@Darens-MBP.lan>
2018-12-02 19:10:29 +1030

    fixed error

--- assets/scripts/create_bibliography.sh
+++ assets/scripts/create_bibliography.sh
@@ -10,5 +10,5 @@ bash ./gather_pdfs.sh dcc_bibliography.csv
 # format full publications markdown page and write to proper location
 cat \
 <(cat yaml_header.txt) \
-<(bash ./format_markdown.sh dcc_bibliography.bib dcc_bibliography.csv \
+<(bash ./format_markdown.sh dcc_bibliography.bib dcc_bibliography.csv) \
 > ../../_pages/publications.md

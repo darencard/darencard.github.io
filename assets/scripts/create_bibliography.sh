53e30dec4619904357f6711e859fb0a054424412
Daren Card <darencard@Darens-MacBook-Pro.local>
2018-12-08 14:15:11 +1030

    first commit for cite summary graphics

--- assets/scripts/create_bibliography.sh
+++ assets/scripts/create_bibliography.sh
@@ -7,8 +7,13 @@ mkdir ../pdfs
 # gather PDFs into directory from my Zotero library
 bash ./gather_pdfs.sh dcc_bibliography.csv
 
+# create citation summary graphics
+cat dcc_bibliography.csv | awk -F "\"\,\"" '{ print $9 }' | sed '/^$/d' > doi.txt
+Rscript produce_citation_summary.R doi.txt ../images/citation_summary.png
+
 # format full publications markdown page and write to proper location
 cat \
 <(cat yaml_header.txt) \
+<(cat citation_summary_markdown.txt) \
 <(bash ./format_markdown.sh dcc_bibliography.bib dcc_bibliography.csv) \
 > ../../_pages/publications.md
